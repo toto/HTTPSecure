32bba6d9af73b9e4d43f737d4f008cb9ec07ad2d
Thomas Kollbach <thomas@kollba.ch>
2010-11-17 17:04:01 +0100

    basic working version that secures facebook

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
-Take a look at the TODO.md file
+HTTPSecure Safari Extention
+===========
 
-This is my first Safari extention and my JavaScript is very rusty. Be gentle and look closely.
+A simple Safari extension that secures Facebook by using *https* instead of *http*. 
+
+This protects agains you against many nasty things that anybody with a bit of knowlege in network securtiy (or [Firesheep](http://codebutler.github.com/firesheep/)) can do.
+
+- Cookie stealing attacks which have been demonstrated so well in [Firesheep](http://codebutler.github.com/firesheep/).
+- Data sniffing
+- Traffic manipulation 
+- and other nasty [Man in the Middle attacks](http://en.wikipedia.org/wiki/Man-in-the-middle_attack)
+
+TODO
+----
+The plan is to extended this to many popular sites (e.g. http*s*://twitter.com). 
+
+Known Problems
+--------------
+
+- First loading is not suppressed. _Workaround:_ You should update your bookmarks, etc. to *https*.
+
+
+Acknowledgements 
+----------------
+
+This Safari Extention is based upon the "Facebook Secure Connection (Force Https SSL)" user script by Merimac (v1.0.11), which can be found [here](http://userscripts.org/scripts/show/86392).
+ 
+License
+------
+
+Licensed as [Creative Commons Attribution-NonCommercial-ShareAlike 3.0 License](http://creativecommons.org/licenses/by-nc-sa/3.0).
--- httpsecure.js
+++ httpsecure.js
@@ -1,77 +1,58 @@
-var HTTPSecure = {
-	"links": function() {
-		return(document.getElementsByTagName("a"));
-	},
+/*
+	httpsecure.js
 	
-	"linkRegEx": function() {
-		var regexStr = "^" + HTTPSecure.insecureProtocol + window.location.host;
-		console.log("regex: " +regexStr);
-		return(new RegExp(regexStr));
-	},
+	Written by Merimac and adapted to Safari by toto. 
 	
-	"insecureProtocol": "http://",
-	"secureProtocol": "https://",
-	"protocolRegEx": /^\w+:/i,
-		
-	"insecureProtocolRegEx": function() {
-		return(new RegExp("^" + HTTPSecure.insecureProtocol));
-	},
-	
+	Source hosted on Github at https://github.com/toto/HTTPSecure
+	License: CC BY-SA-NC 3.0, See README.md for more information.
+*/
 
-	
-	"secureElement": function(element) {
-		var editedAttribute;
-		
-		if (element.nodeName === "A" || element.nodeName === "a") {
-			editedAttribute = "href";
-		} else {
-			if (element.nodeName === "form") {
-				editedAttribute = "action";			
-			} else {
-				console.log("Editing " + element.nodeName);				
-				return false
-			};			
-		};
-		
-		
-		var href = element.getAttribute(editedAttribute);
-		var newAttribute = href;
-		if( href !== null) {
- 			// we have a protocol, but it is http and links to our own site			
-			if(href.match(HTTPSecure.linkRegEx()) !== null) {
-				newAttribute = href.replace(HTTPSecure.insecureProtocolRegEx(),
-																		HTTPSecure.secureProtocol);
-			};
-			
-			// we have a non-protocol refernece (not http:, mailto:, javascript:, etc)
-			// so this is an relative or absolute link
-			if(!href.match(HTTPSecure.protocolRegEx)) {
-				
-				// absolute link
-				if(href.match(/^\//) !== null) {
-					// FIXME: Handle ports or make sure it is port 80
-					newAttribute = "https://" + window.location.host + href;
-				};
-			};
-			console.log("Set new href Attribute: " + newAttribute);
-			element.setAttribute(editedAttribute, newAttribute);			
-			return true;			
-		};
-		
-		
-		
-		return false;
-	}
-};
-
-
-
-var links = HTTPSecure.links();
-// of course for…in does not work on this list. Why would it?
-for (var i = 0; i < links.length; ++i) {
-	HTTPSecure.secureElement(links[i]);
+function $(q, root, single) {
+    if (root && typeof root == 'string') { root = $(root, null, true); }
+    root = root || document;
+    if (q[0]=='#') { return root.getElementById(q.substr(1)); }
+    else if (q[0]=='/' || (q[0]=='.' && q[1]=='/')) {
+        if (single) { return document.evaluate(q, root, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue; }
+        return document.evaluate(q, root, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+    }
+    else if (q[0]=='.') { return root.getElementsByClassName(q.substr(1)); }
+    return root.getElementsByTagName(q);
 }
 
-//for (var i = 0; i < document.forms.length; ++i) {
-//	HTTPSecure.secureElement(document.forms[i]);
-//}
+function fnEnableFacebookHttps() {
+    var url = window.location.href;
+    if(url.indexOf("http://www.facebook.com")==0 ||
+        url.indexOf("http://apps.facebook.com")==0) {
+        window.location.replace(location.href.replace(url.substring(0,7), "https://"));
+    }
+}
+
+function fnEnableHttpsLinks() {
+    var url = window.location.href;
+    if(url.indexOf("https://")==0) {
+        for(var i=0,link; (link=document.links[i]); i++) {
+            if(link.href.indexOf("http://")==0)
+            link.href = link.href.replace(link.href.substring(0,7), "https://");
+        }
+    }
+}
+
+function fnEnableHttpsFacebookLinks(){
+    var links = $("//a[contains(@href,'facebook.com')]");
+//alert(links.snapshotItem(1).href);
+    for (var i=0; i<links.snapshotLength; i++) {
+        links.snapshotItem(i).href = links.snapshotItem(i).href.replace(/^http:\/\/([^\.]*\.)?facebook\.com\/l\.php\?u\=http\%([^\.]*\.)/,'https://$1facebook.com/l.php?u=https%$2');
+        links.snapshotItem(i).href = links.snapshotItem(i).href.replace(/^http:\/\/([^\.]*\.)?facebook\.com\//,'https://$1facebook.com/');
+    }
+}
+
+
+document.addEventListener(
+  'load',
+  function() {
+    fnEnableFacebookHttps(),
+    fnEnableHttpsFacebookLinks(),
+    setTimeout(function() { fnEnableHttpsFacebookLinks() }, 1000);
+  },
+  true
+);
